7e4446b8bfbeab9ed9434df308afe3e149f1c154
James Banday <jamesbanday1@gmail.com>
2026-06-15 21:18:36 -1000

    Add ECR and ACR image publishing workflow

--- terraform/azure/outputs.tf
+++ terraform/azure/outputs.tf
@@ -10,6 +10,10 @@ output "acr_login_server" {
   value = azurerm_container_registry.main.login_server
 }
 
+output "acr_name" {
+  value = azurerm_container_registry.main.name
+}
+
 output "acr_id" {
   value = azurerm_container_registry.main.id
 }
